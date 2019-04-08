07411328e887976d9fcd819cd4628e98a006e695
brain4power <yotakrasnodar@yandex.ru>
2019-04-08 19:17:01 +0300

    init commit lesson3

---
+++ lesson3/client/__main__.py
@@ -0,0 +1,41 @@
+import socket
+from yaml import load, Loader
+from argparse import ArgumentParser
+
+from settings import (
+    ENCODING_NAME, HOST,
+    PORT, BUFFERSIZE
+)
+
+host = HOST
+port = PORT
+encoding_name = ENCODING_NAME
+buffersize = BUFFERSIZE
+
+parser = ArgumentParser()
+parser.add_argument(
+    '-c', '--config', type=str,
+    help='Sets run configuration'
+)
+args = parser.parse_args()
+
+if args.config:
+    with open(args.config) as file:
+        config = load(file, Loader=Loader)
+        host = config.get('host') or HOST
+        port = config.get('port') or PORT
+        encoding_name = config.get('encoding_name') or ENCODING_NAME
+        buffersize = config.get('buffersize') or BUFFERSIZE
+
+try:
+    sock = socket.socket()
+    sock.connect((host, port))
+    print(f'Client started with {host}:{port}')
+    while True:
+        value = input('Enter data to send:')
+        bvalue = value.encode(encoding_name)
+        sock.send(bvalue)
+        data = sock.recv(buffersize)
+        print(data.decode(encoding_name))
+except KeyboardInterrupt:
+    print('Client closed')
