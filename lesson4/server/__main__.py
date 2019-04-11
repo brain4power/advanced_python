b212db22e00bb556fcf31385366a05fd2cbb6de1
brain4power <yotakrasnodar@yandex.ru>
2019-04-11 10:53:51 +0300

    lesson4 init commit

---
+++ lesson4/server/__main__.py
@@ -0,0 +1,76 @@
+import json
+import socket
+from yaml import load, Loader
+from argparse import ArgumentParser
+
+import settings
+
+from routes import resolve
+from protocol import (
+    validate_request, make_response,
+    make_400, make_404
+)
+from settings import (
+    ENCODING_NAME, HOST,
+    PORT, BUFFERSIZE
+)
+
+
+host = HOST
+port = PORT
+encoding_name = ENCODING_NAME
+buffersize = BUFFERSIZE
+
+parser = ArgumentParser()
+parser.add_argument(
+    '-c', '--config', type=str,
+    help='Sets run configuration'
+)
+args = parser.parse_args()
+
+if args.config:
+    with open(args.config) as file:
+        config = load(file, Loader=Loader)
+        host = config.get('host') or HOST
+        port = config.get('port') or PORT
+        encoding_name = config.get('encoding_name') or ENCODING_NAME
+        buffersize = config.get('buffersize') or BUFFERSIZE
+
+try:
+    sock = socket.socket()
+    sock.bind((host, port))
+    sock.listen(5)
+    print(f'Server started with { host }:{ port }')
+    while True:
+        client, address = sock.accept()
+        print(f'Client detected { address }')
+        b_request = client.recv(buffersize)
+
+        request = json.loads(
+            b_request.decode(ENCODING_NAME)
+        )
+
+        action_name = request.get('action')
+
+        if validate_request(request):
+            controller = resolve(action_name)
+            if controller:
+                try:
+                    response = controller(request)
+                except Exception as err:
+                    print(err)
+                    response = make_response(
+                        request, 500, 'Internal server error'
+                    )
+            else:
+                print('Action { action_name } does not exits')
+                response = make_404(request)
+        else:
+            print('Request is not valid')
+            response = make_400(request)
+            
+        s_response = json.dumps(response)
+        client.send(s_response.encode(ENCODING_NAME))
+        
+except KeyboardInterrupt:
+    print('Client closed')
---
+++ lesson4/server/echo/routes.py
@@ -0,0 +1,7 @@
+from .controller import (
+    get_echo
+)
+
+routes = [
+    {'echo': 'now', 'controller': get_echo}
+]
