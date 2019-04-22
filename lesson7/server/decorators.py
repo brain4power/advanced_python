1fb22899ba43512c139db38b3573cea9bab41bc6
brain4power <yotakrasnodar@yandex.ru>
2019-04-22 13:00:25 +0300

    добавил декоратору декоратор @wraps из functools для корректного отображения имени функции в логах

--- lesson7/server/decorators.py
+++ lesson7/server/decorators.py
@@ -1,10 +1,12 @@
 import logging
+from functools import wraps
 
 
 logger = logging.getLogger('decorators')
 
 
 def logged(func):
+    @wraps(func)
     def wrapper(request, *args, **kwargs):
         logger.debug(f'{ func.__name__ } - { request }, args= {args}')
         return func(request, *args, **kwargs)
